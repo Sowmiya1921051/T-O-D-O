6ebfb168cda29540bef6541ffef05ee62a9e2f9d
sowmiya <sowmiyanallasivam3434@gmail.com>
2023-12-05 18:04:38 +0530

    Enter the items

--- src/Buy.jsx
+++ src/Buy.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 
 function Buy() {
-    const [selectedOption, setSelectedOption] = useState('');
+    const [add, setAdd] = useState('');
+    const [items, setItems] = useState([]);
 
-    const handleChange = (event) => {
-        setSelectedOption(event.target.value);
+    const func = () => {
+
+        setItems(prevItems => [...prevItems, add]);
+        
+        setAdd('');
     };
 
     return (
@@ -12,17 +16,34 @@ function Buy() {
             <div className='text-2xl font-bold text-center'>Todo shop</div>
             <div className='text-center'>
                 <p>Select what you want? </p>
-                <select onChange={handleChange} value={selectedOption}>
+                <select>
                     <option value="pencil">pencil</option>
                     <option value="paper">paper</option>
                     <option value="notepad">notepad</option>
                     <option value="pen">pen</option>
-                    <option value="notebook">notebook</option>
+                    <option value="note book">note book</option>
                 </select>
             </div>
-                <div className='text-center mt-4'>
-                    <p> selected : {selectedOption}</p>
-                </div>
+           
+            <div className='text-center'>
+                <input
+                    type='text'
+                    value={add}
+                    onChange={(e) => setAdd(e.target.value)}
+                    placeholder='Type and click add'
+                />
+                <button onClick={func}>Add</button>
+                <div>
+                <p>Added items:</p>
+                <ul>
+                    {items.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </div>
+            <div>Discount 5%</div>
+            </div>
+           
             
         </div>
     );
