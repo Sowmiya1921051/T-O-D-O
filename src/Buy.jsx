4b8326f46701469edf3ef673baa9a3775a76ad1f
sowmiya <sowmiyanallasivam3434@gmail.com>
2023-12-05 15:53:22 +0530

    Show the value

--- src/Buy.jsx
+++ src/Buy.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Buy() {
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+
     return (
         <div>
             <div className='text-2xl font-bold text-center'>Todo shop</div>
-            <div></div>
-            <select>
-               <option value="pencil">pencil </option>
-               <option value="pencil">pencil </option>
-               <option value="pencil">pencil </option>
-               <option value="pencil">pencil </option>
-               <option value="pencil">pencil </option>
-            </select>
+            <div className='text-center'>
+                <p>Select what you want? </p>
+                <select onChange={handleChange} value={selectedOption}>
+                    <option value="pencil">pencil</option>
+                    <option value="paper">paper</option>
+                    <option value="notepad">notepad</option>
+                    <option value="pen">pen</option>
+                    <option value="notebook">notebook</option>
+                </select>
+            </div>
+                <div className='text-center mt-4'>
+                    <p>Hii Sowmia, you selected: {selectedOption}</p>
+                </div>
+            
         </div>
-    )
+    );
 }
 
-export default Buy
+export default Buy;
